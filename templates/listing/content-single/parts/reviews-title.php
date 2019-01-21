29dced84da892aaf0d17ae869b37b350f4b13842
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-01-21 19:36:40 +0100

    Update language file

--- templates/listing/content-single/parts/reviews-title.php
+++ templates/listing/content-single/parts/reviews-title.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<h2><?php esc_html_e( 'Reviews', 'hivepress' ); ?></h2>
+<h2><?php esc_html_e( 'Reviews', 'hivepress-reviews' ); ?></h2>
