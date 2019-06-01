8e0ac167eb5ba677c73c8a7483298332e0effde4
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-06-01 23:14:06 +0200

    Add initial files

---
+++ hivepress-reviews.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: HivePress Reviews
+ * Description: Reviews extension for HivePress plugin.
+ * Version: 1.1.0
+ * Author: HivePress
+ * Author URI: https://hivepress.io/
+ * Text Domain: hivepress-reviews
+ * Domain Path: /languages/
+ *
+ * @package HivePress
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+// Register plugin directory.
+add_filter(
+	'hivepress/v1/dirs',
+	function( $dirs ) {
+		return array_merge( $dirs, [ __DIR__ ] );
+	}
+);
