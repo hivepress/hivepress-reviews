78106de7efd938326d38e63d1116182668f291d5
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-06-02 20:08:12 +0200

    Fix rating calculation

--- includes/components/class-review.php
+++ includes/components/class-review.php
@@ -8,6 +8,7 @@
 namespace HivePress\Components;
 
 use HivePress\Helpers as hp;
+use HivePress\Models;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
@@ -24,11 +25,96 @@ final class Review {
 	 */
 	public function __construct() {
 
+		// Update rating.
+		add_action( 'wp_insert_comment', [ $this, 'update_rating' ] );
+		add_action( 'wp_set_comment_status', [ $this, 'update_rating' ] );
+		add_action( 'delete_comment', [ $this, 'update_rating' ] );
+
 		// Delete reviews.
 		add_action( 'delete_user', [ $this, 'delete_reviews' ] );
+	}
 
-		if ( ! is_admin() ) {
+	/**
+	 * Gets rating.
+	 *
+	 * @param array $args Query arguments.
+	 * @return array
+	 */
+	protected function get_rating( $args ) {
 
+		// Get review IDs.
+		$review_ids = get_comments(
+			array_merge(
+				[
+					'type'   => 'hp_review',
+					'status' => 'approve',
+					'fields' => 'ids',
+				],
+				$args
+			)
+		);
+
+		// Get rating count.
+		$rating_count = count( $review_ids );
+		$rating_value = null;
+
+		// Calculate rating.
+		foreach ( $review_ids as $review_id ) {
+			$rating = absint( get_comment_meta( $review_id, 'hp_rating', true ) );
+
+			if ( $rating < 1 ) {
+				$rating = 1;
+			} elseif ( $rating > 5 ) {
+				$rating = 5;
+			}
+
+			$rating_value += $rating;
+		}
+
+		if ( $rating_count > 0 ) {
+			$rating_value = round( $rating_value / $rating_count, 1 );
+
+			return [ $rating_value, $rating_count ];
+		}
+
+		return null;
+	}
+
+	/**
+	 * Updates rating.
+	 *
+	 * @param int $review_id Review ID.
+	 */
+	public function update_rating( $review_id ) {
+
+		// Get review.
+		$review = Models\Review::get( $review_id );
+
+		if ( ! is_null( $review ) ) {
+
+			// Update listing rating.
+			$listing_rating = $this->get_rating( [ 'post_id' => $review->get_listing_id() ] );
+
+			if ( ! is_null( $listing_rating ) ) {
+				update_post_meta( $review->get_listing_id(), 'hp_rating', reset( $listing_rating ) );
+			} else {
+				delete_post_meta( $review->get_listing_id(), 'hp_rating' );
+			}
+
+			// Get vendor ID.
+			$vendor_id = wp_get_post_parent_id( $review->get_listing_id() );
+
+			if ( $vendor_id ) {
+
+				// Update vendor rating.
+				$vendor_rating = $this->get_rating( [ 'post_parent' => $vendor_id ] );
+
+				if ( ! is_null( $vendor_rating ) ) {
+					update_post_meta( $vendor_id, 'hp_rating', reset( $vendor_rating ) );
+				} else {
+					delete_post_meta( $vendor_id, 'hp_rating' );
+				}
+			}
 		}
 	}
 
