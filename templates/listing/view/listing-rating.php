f4e6965454fc785c2692ab27136ae54133d2bb00
Ihor <ihor@hivepress.io>
2020-07-14 19:24:01 +0300

    Make rating count clickable

--- templates/listing/view/listing-rating.php
+++ templates/listing/view/listing-rating.php
@@ -6,10 +6,10 @@ if ( $listing->get_rating() ) :
 	?>
 	<div class="hp-listing__rating hp-rating">
 		<div class="hp-rating__stars hp-rating-stars" data-component="rating" data-value="<?php echo esc_attr( $listing->get_rating() ); ?>"></div>
-		<div class="hp-rating__details">
+		<a href="<?php echo esc_url( hivepress()->router->get_url( 'listing_view_page', [ 'listing_id' => $listing->get_id() ] ) ); ?>#reviews" class="hp-rating__details">
 			<span class="hp-rating__value"><?php echo esc_html( $listing->display_rating() ); ?></span>
 			<span class="hp-rating__count">(<?php echo esc_html( $listing->display_rating_count() ); ?>)</span>
-		</div>
+		</a>
 	</div>
 	<?php
 endif;
