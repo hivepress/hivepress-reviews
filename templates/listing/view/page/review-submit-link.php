0396d29f801a3de152b5220bb0114e1f532d61f1
Ihor <ihor@hivepress.io>
2021-02-12 23:24:43 +0200

    Fix templates

--- templates/listing/view/page/review-submit-link.php
+++ templates/listing/view/page/review-submit-link.php
@@ -2,4 +2,7 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<a href="#<?php if ( is_user_logged_in() ) : ?>review_submit<?php else : ?>user_login<?php endif; ?>_modal" class="hp-listing__action hp-listing__action--review hp-link"><i class="hp-icon fas fa-star"></i><span><?php esc_html_e( 'Write a Review', 'hivepress-reviews' ); ?></span></a>
+<a href="#<?php if ( is_user_logged_in() ) : ?>review_submit<?php else : ?>user_login<?php endif; ?>_modal" class="hp-listing__action hp-listing__action--review hp-link">
+	<i class="hp-icon fas fa-star"></i>
+	<span><?php esc_html_e( 'Write a Review', 'hivepress-reviews' ); ?></span>
+</a>
