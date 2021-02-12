aa58264b8220b2f8e707e6c24fc50d84d020e664
Ihor <ihor@hivepress.io>
2021-02-12 23:22:48 +0200

    Fix templates

--- templates/review/view/review-created-date.php
+++ templates/review/view/review-created-date.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<time class="hp-review__created-date hp-review__date" datetime="<?php echo esc_attr( $review->get_created_date() ); ?>"><?php echo esc_html( $review->display_created_date() ); ?></time>
+<time class="hp-review__created-date hp-review__date hp-meta" datetime="<?php echo esc_attr( $review->get_created_date() ); ?>"><?php echo esc_html( $review->display_created_date() ); ?></time>
