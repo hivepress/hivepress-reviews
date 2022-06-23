cb0255f53912cf7c040c81e3e0dcc5803fd12e63
ihor <ihor@hivepress.io>
2022-06-23 03:52:09 +0300

    Add reply link

---
+++ templates/review/view/review-reply-link.php
@@ -0,0 +1,12 @@
+<?php
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! $review->get_parent__id() && $listing->get_user__id() === get_current_user_id() ) :
+	?>
+	<a href="#review_reply_modal_<?php echo esc_attr( $review->get_id() ); ?>" class="hp-review__action hp-review__action--reply hp-link">
+		<i class="hp-icon fas fa-share"></i>
+		<span><?php esc_html_e( 'Reply', 'hivepress-reviews' ); ?></span>
+	</a>
+	<?php
+endif;
