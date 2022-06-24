ec88841ae81061eba5545d86f73ae3fae3d7232e
ihor <ihor@hivepress.io>
2022-06-25 00:38:11 +0300

    Fix templates

--- templates/review/view/review-rating.php
+++ templates/review/view/review-rating.php
@@ -2,7 +2,7 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-if ( $review->has_rating() ) :
+if ( ! $review->get_parent__id() ) :
 	?>
 	<div class="hp-review__rating hp-rating-stars" data-component="rating" data-value="<?php echo esc_attr( $review->get_rating() ); ?>"></div>
 	<?php
