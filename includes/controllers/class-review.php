1a77cfbab1da144aede3847ecbbaa5bab63009be
ihor <ihor@hivepress.io>
2022-06-27 14:11:35 +0300

    Revert changes

--- includes/controllers/class-review.php
+++ includes/controllers/class-review.php
@@ -41,14 +41,6 @@ final class Review extends Controller {
 						'action' => [ $this, 'submit_review' ],
 						'rest'   => true,
 					],
-
-					'review_reply_action'  => [
-						'base'   => 'reviews_resource',
-						'path'   => '/reply',
-						'method' => 'POST',
-						'action' => [ $this, 'submit_reply' ],
-						'rest'   => true,
-					],
 				],
 			],
 			$args
@@ -145,81 +137,6 @@ final class Review extends Controller {
 					'author__display_name' => $author->get_display_name(),
 					'author__email'        => $author->get_email(),
 					'approved'             => get_option( 'hp_review_enable_moderation' ) ? 0 : 1,
-				]
-			)
-		);
-
-		if ( ! $review->save() ) {
-			return hp\rest_error( 400, $review->_get_errors() );
-		}
-
-		return hp\rest_response(
-			201,
-			[
-				'id' => $review->get_id(),
-			]
-		);
-	}
-
-	/**
-	 * Submits reply.
-	 *
-	 * @param WP_REST_Request $request API request.
-	 * @return WP_Rest_Response
-	 */
-	public function submit_reply( $request ) {
-
-		// Check option.
-		if ( ! get_option( 'hp_review_allow_replies' ) ) {
-			return hp\rest_error( 400 );
-		}
-
-		// Check authentication.
-		if ( ! is_user_logged_in() ) {
-			return hp\rest_error( 401 );
-		}
-
-		// Validate form.
-		$form = ( new Forms\Review_Reply() )->set_values( $request->get_params() );
-
-		if ( ! $form->validate() ) {
-			return hp\rest_error( 400, $form->get_errors() );
-		}
-
-		// Get author.
-		$author_id = $request->get_param( 'author' ) ? $request->get_param( 'author' ) : get_current_user_id();
-
-		$author = Models\User::query()->get_by_id( $author_id );
-
-		if ( ! $author ) {
-			return hp\rest_error( 400 );
-		}
-
-		// Get parent review.
-		$parent_review = Models\Review::query()->get_by_id( $form->get_value( 'parent' ) );
-
-		if ( ! $parent_review || ! $parent_review->get_approved() ) {
-			return hp\rest_error( 400 );
-		}
-
-		// Get listing.
-		$listing = $parent_review->get_listing();
-
-		if ( ! $listing || $listing->get_status() !== 'publish' || $listing->get_user__id() !== $author_id ) {
-			return hp\rest_error( 400 );
-		}
-
-		// Add review.
-		$review = ( new Models\Review() )->fill(
-			array_merge(
-				$form->get_values(),
-				[
-					'author'               => $author->get_id(),
-					'author__display_name' => $author->get_display_name(),
-					'author__email'        => $author->get_email(),
-					'approved'             => get_option( 'hp_review_enable_moderation' ) ? 0 : 1,
-					'listing'              => $listing->get_id(),
-					'parent'               => $parent_review->get_id(),
 				]
 			)
 		);
