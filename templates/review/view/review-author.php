3087b85392ee673a0c9bd877bb0e8d11e5ca4c95
HivePress <43493187+hivepress@users.noreply.github.com>
2023-09-28 23:54:59 +0200

    Make usernames clickable

--- templates/review/view/review-author.php
+++ templates/review/view/review-author.php
@@ -1,5 +1,22 @@
 <?php
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+$display = get_option( 'hp_user_enable_display' );
 ?>
-<div class="hp-review__author"><?php echo esc_html( $review->get_author__display_name() ); ?></div>
+<div class="hp-review__author">
+	<?php
+	if ( $display ) : ?>
+		<a href="<?php echo esc_url( hivepress()->router->get_url( 'user_view_page', [ 'username' => $review->get_author__username() ] ) ); ?>">
+		<?php
+	endif;
+
+	echo esc_html( $review->get_author__display_name() );
+
+	if ( $display ) :
+		?>
+		</a>
+		<?php
+	endif;
+	?>
+</div>
