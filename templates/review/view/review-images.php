2bbf37a5fa10e312614ff19667cc9c7c0808064b
HivePress <43493187+hivepress@users.noreply.github.com>
2025-10-10 17:50:47 +0200

    Fix image styles

--- templates/review/view/review-images.php
+++ templates/review/view/review-images.php
@@ -2,11 +2,17 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-if ( $review->get_images__id() ) :
+if ( $review->get_images__id() && ! empty( $listing ) ) :
+	$images = is_array( $review->get_images__id() ) ? $review->get_images() : [ $review->get_images() ];
 	?>
-	<a href="<?php echo esc_url( $review->get_images__url() ); ?>" target="_blank" class="hp-review__images hp-link">
-		<i class="hp-icon fas fa-file-download"></i>
-		<span><?php echo esc_html( $review->get_images__name() ); ?></span>
-	</a>
+	<div class="hp-review__images">
+		<div class="hp-row">
+			<?php foreach ( $images as $image ) : ?>
+				<div class="hp-image hp-col-sm-2 hp-col-xs-4">
+					<img src="<?php echo esc_url( $image->get_url( 'thumbnail' ) ); ?>" data-component="image" data-zoom="<?php echo esc_url( $image->get_url( 'large' ) ); ?>" alt="<?php echo esc_attr( $listing->get_title() ); ?>" loading="lazy">
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
 	<?php
 endif;
